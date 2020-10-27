39e98d7341c0fa26de88dd9b36314dfa49af9aa9
YuriiOL <67682937+YuriiOL@users.noreply.github.com>
2020-10-27 16:11:34 +0200

    Homework_1

---
+++ JS/Homework/Homework_1/script.js
@@ -0,0 +1,294 @@
+//1
+// - присвоить каждому из следующих значений свою переменную: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false Вывести каждую при помощи console.log , alert, document.write
+// let hello = 'hello'
+// let owu = 'owu'
+// let com = 'com'
+// let ua = 'ua'
+// let one = 1
+// let ten = 10
+// let oneHundred = 100
+// let minus = -999
+// let oneDusTres = 123
+// let pi = 3.14
+// let e = 2.7
+// let age = 16
+// let ok = true
+// let no = false
+
+// console.log(hello)
+// console.log(owu)
+// console.log(com)
+// console.log(ua)
+// console.log(one)
+// console.log(ten)
+// console.log(oneHundred)
+// console.log(minus)
+// console.log(oneDusTres)
+// console.log(pi)
+// console.log(e)
+// console.log(age)
+// console.log(ok)
+// console.log(no)
+
+// alert(hello)
+// alert(owu)
+// alert(com)
+// alert(ua)
+// alert(one)
+// alert(ten)
+// alert(oneHundred)
+// alert(minus)
+// alert(oneDusTres)
+// alert(pi)
+// alert(e)
+// alert(age)
+// alert(ok)
+// alert(no)
+
+// document.write(hello)
+// document.write(owu)
+// document.write(com)
+// document.write(ua)
+// document.write(one)
+// document.write(ten)
+// document.write(oneHundred)
+// document.write(minus)
+// document.write(oneDusTres)
+// document.write(pi)
+// document.write(e)
+// document.write(age)
+// document.write(ok)
+// document.write(no)
+//2
+// - переопределить каждую переменную из задания 1 дав им произвольные значения Вывести каждую при помощи console.log , alert, document.write
+
+// hello = 'Privet'
+// owu = 'Auf'
+// com = 'www'
+// ua = 'ru'
+// one = 3
+// ten = 30
+// oneHundred = 300
+// minus = -333
+// oneDusTres = 333
+// pi = 3.33333
+// e = 3.3
+// age = 16.3333333
+// ok = false
+// no = true
+
+// console.log(hello)
+// console.log(owu)
+// console.log(com)
+// console.log(ua)
+// console.log(one)
+// console.log(ten)
+// console.log(oneHundred)
+// console.log(minus)
+// console.log(oneDusTres)
+// console.log(pi)
+// console.log(e)
+// console.log(age)
+// console.log(ok)
+// console.log(no)
+
+// alert(hello)
+// alert(owu)
+// alert(com)
+// alert(ua)
+// alert(one)
+// alert(ten)
+// alert(oneHundred)
+// alert(minus)
+// alert(oneDusTres)
+// alert(pi)
+// alert(e)
+// alert(age)
+// alert(ok)
+// alert(no)
+
+// document.write(hello)
+// document.write(owu)
+// document.write(com)
+// document.write(ua)
+// document.write(one)
+// document.write(ten)
+// document.write(oneHundred)
+// document.write(minus)
+// document.write(oneDusTres)
+// document.write(pi)
+// document.write(e)
+// document.write(age)
+// document.write(ok)
+// document.write(no)
+
+//3
+// - Создать 3 числовых и 3 стринговых константы. Вывести каждую при помощи console.log , alert, document.write
+// const str1 = 'Yurii'
+// const str2 = 'Oleksovskyi'
+// const str3 = '22'
+// const num1 = 11
+// const num2 = 22
+// const num3 = 33
+
+// console.log(str1)
+// console.log(str2)
+// console.log(str3)
+// console.log(num1)
+// console.log(num2)
+// console.log(num3)
+
+// alert(str1)
+// alert(str2)
+// alert(str3)
+// alert(num1)
+// alert(num2)
+// alert(num3)
+
+// document.write(str1)
+// document.write(str2)
+// document.write(str3)
+// document.write(num1)
+// document.write(num2)
+// document.write(num3)
+
+//4
+//- при помощи 3х разных prompt() получить 3 слова которые являются вашими ФИО. Для фамилии имени и отчества создать разные переменные. Вывести каждую при помощи console.log , alert, document.write
+
+// let name = prompt('Name:')
+// let surname = prompt('Surname:')
+// let secondName = prompt('SecondName')
+
+// console.log(name)
+// console.log(surname)
+// console.log(secondName)
+
+// alert(name)
+// alert(surname)
+// alert(secondName)
+
+// document.write(name)
+// document.write(surname)
+// document.write(secondName)
+
+//5
+//- Взять переменные из задания 4 и сконкатенировать их в одной переменной person
+
+// let name = prompt('Name:')
+// let surname = prompt('Surname:')
+// let secondName = prompt('SecondName')
+// let person = name + ' ' + surname + ' ' + secondName
+// console.log(person)
+
+//6
+//- Взять задние 4 и 5 и применить его к ФИО всех членов своей семьи.
+
+// let myname = prompt('Name:')
+// let mysurname = prompt('Surname:')
+// let mysecondName = prompt('SecondName')
+// let meperson = myname + ' ' + mysurname + ' ' + mysecondName
+// let momname = prompt('Mom Name:')
+// let momsurname = prompt('Mom Surname:')
+// let momsecondName = prompt('Mom SecondName')
+// let momperson = momname + ' ' + momsurname + ' ' + momsecondName
+// let dadname = prompt('Dad Name:')
+// let dadsurname = prompt('Dad Surname:')
+// let dadsecondName = prompt('Dad SecondName')
+// let dadperson = dadname + ' ' + dadsurname + ' ' + dadsecondName
+// console.log(meperson)
+// console.log(momperson)
+// console.log(dadperson)
+
+//7
+//- при помощи prompt() получить 3 числа. Привести их к числовому типу при помощи +. вывести их в консоль.
+
+// let number1 = prompt('Numb 1:')
+// let number2 = prompt('Numb 2:')
+// let number3 = prompt('Numb 3:')
+// console.log(+number1, +number2, +number3)
+
+//8
+//- при помощи prompt() получить 4 числа. Привести их к числовому типу при помощи parseInt. Сложить их между собой записав результат в переменную result и вывести в консоль браузера
+
+// let number1 = parseInt(prompt('Numb 1:'))
+// let number2 = parseInt(prompt('Numb 2:'))
+// let number3 = parseInt(prompt('Numb 3:'))
+// let number4 = parseInt(prompt('Numb 4:'))
+// let result = number1 + number2 + number3 + number4
+// console.log(result)
+
+//9
+//- при помощи prompt()  получить 3 числа с плавающей точекой. при помощи parseFloat привести их к соответсвующему типу. Сложить их между собой записав результат в переменную result и вывести в консоль браузера
+
+// let number1 = parseFloat(prompt('Numb 1:'))
+// let number2 = parseFloat(prompt('Numb 2:'))
+// let number3 = parseFloat(prompt('Numb 3:'))
+// let result = number1 + number2 + number3
+// console.log(result)
+
+//10
+//- при помощи prompt()  получить 3 числа с плавающей точекой. Округлить их при помощи Math.round Сложить их между собой записав результат в переменную  и вывести в консоль браузера
+
+// let number1 = Math.round(prompt('Numb 1:'))
+// let number2 = Math.round(prompt('Numb 2:'))
+// let number3 = Math.round(prompt('Numb 3:'))
+// let result = number1 + number2 + number3
+// console.log(result)
+
+//11
+//- при помощи prompt()  получить 2 целых числа. Привести их к целочисленному типу. Первое число - это число которое будут возводить в степень. Второе число - это число которое является степенью. При помощи Math.pow возвести первое число в степень второго числа.
+
+// let number1 = Math.round(prompt('Number1:'))
+// let number2 = Math.round(prompt('Number2:'))
+// let result = Math.pow(number1, number2)
+// console.log(result)
+
+//12
+//- При помощи оператора опредеоения типа typeof определить типы следующих переменных и вывести их в консоль let a = 100; let b = '100'; let c = true; let d = undefined;
+
+// let a = 100
+// let b = '100'
+// let c = true
+// let d = undefined
+// console.log(typeof a)
+// console.log(typeof b)
+// console.log(typeof c)
+// console.log(typeof d)
+
+//13
+// - Поставьте соответствующий оператор в выражениях что бы получился соответсвующий результат.
+//  В одинаковых выражениях в задании не использовать два одинаковых оператора.
+// Каждое выражение вложить в свою переменную
+
+// console.log(5 < 6)
+// console.log(5 > 6)
+// console.log(5 >= 6)
+// console.log(5 >= 6)
+// console.log(10 === 10)
+// console.log(10 == 10)
+// console.log(10 > 10)
+// console.log(10 < 10)
+// console.log(10 !== 10)
+// console.log(123 === '123')
+// console.log(123 == '123')
+
+//14
+// Дополнительно:
+// - Посмотрев на следюующие выражения, скажите, каков будет вывод в консоль
+console.log(132 > 100 && 45 < 12) //false
+console.log(34 > 33 && 23 < 90) //true
+console.log(99 > 100 && 45 > 12) //false
+console.log(132 > 100 || 45 < 12) //true
+console.log(111 > 11 || 45 < 111) //true
+console.log((111 > 11 || 45 < 111) && (132 > 100 || 45 < 12)) //true
+console.log(111 > 11 || 45 < 56 || 132 > 100 || 45 < 12) //true
+console.log(132 > 100 && 45 < 12 && (132 > 100 || 45 < 12)) //false
+console.log(111 > 11 || 45 < 111 || (99 > 100 && 45 > 12)) //true
+console.log(!!'-1')
+console.log(!!-1)
+console.log(!!'0')
+console.log(!!'null')
+console.log(!!'undefined')
+console.log(!!(3 / 'owu'))
+console.log(111 > 11 || 45 < 111 || !!'0')
+console.log((!!111 == !!11 || 45 < 111) && 99 > 100 && 45 > 12)
