37921d40a832d13d68552450cdf7f72fa0aead15
YuriiOL <67682937+YuriiOL@users.noreply.github.com>
2020-12-27 15:11:44 +0200

    homework 7

--- REACT/template/src/App.js
+++ REACT/template/src/App.js
@@ -1,83 +1,28 @@
-import React, { memo, useEffect, useMemo, useReducer, useState} from 'react'
+import React, {useEffect} from "react";
 
-const reducer = (state, action) => {
-    switch (action.type){
-        case "SET_TODO": {
-            return action.payload
-        }
+import {useDispatch, useSelector} from 'react-redux';
 
-        case "CHANGE_TODO_STATUS" : {
-            return {
-                ...state,
-                completed: !state.completed
-            }
-        }
-        case "CHANGE_TODO_TITLE": {
-            return{
-                ...state,
-                title: action.payload
-            }
-        }
-        default: {
-            return state
-        }
-    }
-}
-const initialState = {
-    userId: null,
-    id: null,
-    title: '',
-    completed: false
-}
 
-export  default function App() {
-    const [state, dispatch] = useReducer(reducer, initialState)
-
-    const [user, setUser] = useState({name: 'Yurii', age: 24})
-    const [counter, setCounter] = useState(1)
-
-    useEffect(()=>{
-        console.log('Iwas called')
-        fetch(`https://jsonplaceholder.typicode.com/todos/${counter}`)
-            .then(response => response.json())
-            .then(json => {
-                dispatch({type: "SET_TODO", payload: json})
-                console.log(json)
-            })
+export default function App() {
+    const counter = useSelector(({counter}) => counter)
+    const todo = useSelector(({todo}) => todo)
+    const dispatch = useDispatch();
+    useEffect(() => {
+            fetch(`https://jsonplaceholder.typicode.com/todos/${counter}`)
+                .then(value => value.json())
+                .then(value => dispatch({type: 'SET_TODO', payload: value}))
     }, [counter])
-
-    const AddSome = () => {
-        // setCounter(counter + 1)
-        // setUser((val)=>({
-        //     ...val,
-        //     age: val.age + 1
-        // }))
-        setCounter(val => val + 1)
-    }
-    const ChangeStatus = () => {
-        dispatch({ type: "CHANGE_TODO_STATUS"})
-    }
-    const TitleChange = () => {
-        dispatch({ type: "CHANGE_TODO_TITLE", payload: Math.random()})
-    }
-    return(
-        <div className='App'>
-
-            <h1>Hello</h1>
-            <p>My name {user.name}</p>
-            <p>My age {user.age}</p>
-            {!!state && (
-                <div>
-                    <p>{state.id}</p>
-                    <p>{state.title}</p>
-                    <p>{state.completed.toString()}</p>
-                </div>
-            )
-            }
-
-            <button onClick={AddSome}>Add</button>
-            <button onClick={ChangeStatus}>ChangeStatus</button>
-            <button onClick={TitleChange}>ChangeTitle</button>
+    return (
+        <div>
+            <h1>Counter {counter}</h1>
+            <button onClick={() => dispatch({type: 'INC_COUNTER'})}>add</button>
+            <button onClick={() => dispatch({type: 'DEC_COUNTER'})}>dec</button>
+            <button onClick={() => dispatch({type: 'RESET'})}>reset</button>
+            {todo && <div>
+                <h3>{todo.id} </h3>
+                <h3>{todo.title} </h3>
+                <h3>{todo.completed.toString()}</h3>
+            </div>}
         </div>
-    )
+    );
 }
--- REACT/template/src/index.js
+++ REACT/template/src/index.js
@@ -3,13 +3,45 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from "redux";
+import {Provider} from "react-redux";
+
+const initialState = {
+    counter: 1,
+    todo : null
+}
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'INC_COUNTER' : {
+            return {...state, counter: state.counter + 1}
+        }
+        case 'DEC_COUNTER' : {
+            return {...state, counter: state.counter - 1}
+        }
+        case 'RESET' : {
+            return {...state, counter: 0}
+        }
+        case 'SET_TODO' : {
+            return {...state, todo: action.payload}
+        }
+        default: {
+            return state;
+        }
+    }
+}
+
+const store = createStore(reducer);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
