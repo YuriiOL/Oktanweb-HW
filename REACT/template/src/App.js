915d2b5abd2794667c18f5c4f162da0889c85927
YuriiOL <67682937+YuriiOL@users.noreply.github.com>
2020-12-27 13:42:29 +0200

    lesson 7

--- REACT/template/src/App.js
+++ REACT/template/src/App.js
@@ -1,83 +1,14 @@
-import React, { memo, useEffect, useMemo, useReducer, useState} from 'react'
-
-const reducer = (state, action) => {
-    switch (action.type){
-        case "SET_TODO": {
-            return action.payload
-        }
-
-        case "CHANGE_TODO_STATUS" : {
-            return {
-                ...state,
-                completed: !state.completed
-            }
-        }
-        case "CHANGE_TODO_TITLE": {
-            return{
-                ...state,
-                title: action.payload
-            }
-        }
-        default: {
-            return state
-        }
-    }
-}
-const initialState = {
-    userId: null,
-    id: null,
-    title: '',
-    completed: false
-}
-
+import React from 'react'
+import {useSelector, useDispatch} from 'react-redux'
 export  default function App() {
-    const [state, dispatch] = useReducer(reducer, initialState)
-
-    const [user, setUser] = useState({name: 'Yurii', age: 24})
-    const [counter, setCounter] = useState(1)
-
-    useEffect(()=>{
-        console.log('Iwas called')
-        fetch(`https://jsonplaceholder.typicode.com/todos/${counter}`)
-            .then(response => response.json())
-            .then(json => {
-                dispatch({type: "SET_TODO", payload: json})
-                console.log(json)
-            })
-    }, [counter])
-
-    const AddSome = () => {
-        // setCounter(counter + 1)
-        // setUser((val)=>({
-        //     ...val,
-        //     age: val.age + 1
-        // }))
-        setCounter(val => val + 1)
-    }
-    const ChangeStatus = () => {
-        dispatch({ type: "CHANGE_TODO_STATUS"})
-    }
-    const TitleChange = () => {
-        dispatch({ type: "CHANGE_TODO_TITLE", payload: Math.random()})
-    }
+    const counter = useSelector(({counter})=> counter)
+    const dispatch = useDispatch()
     return(
         <div className='App'>
-
-            <h1>Hello</h1>
-            <p>My name {user.name}</p>
-            <p>My age {user.age}</p>
-            {!!state && (
-                <div>
-                    <p>{state.id}</p>
-                    <p>{state.title}</p>
-                    <p>{state.completed.toString()}</p>
-                </div>
-            )
-            }
-
-            <button onClick={AddSome}>Add</button>
-            <button onClick={ChangeStatus}>ChangeStatus</button>
-            <button onClick={TitleChange}>ChangeTitle</button>
+           <h1>Counter {counter}</h1>
+            <button onClick={()=>dispatch({type: 'INC_COUNTER'})}>ADD</button>
+            <button onClick={()=>dispatch({type: 'DEC_COUNTER'})}>DEC</button>
+            <button onClick={()=>dispatch({type: 'RESET'})}>RES</button>
         </div>
     )
 }
--- REACT/template/src/index.js
+++ REACT/template/src/index.js
@@ -3,10 +3,41 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from "redux";
+import {Provider} from "react-redux";
+
+const initialState = {
+    counter: 0,
+    user: {name: 'test', age: 254}
+}
+const reducer = (state = initialState, action) => {
+    switch (action.type){
+        case 'INC_COUNTER': {
+            return {...state, counter: state.counter + 1}
+        }
+        case 'DEC_COUNTER': {
+            return {...state, counter: state.counter - 1}
+        }
+        case 'CHANGE_USER': {
+            return {...state, user:{...state.user, age: 12 } }
+        }
+        case 'RESET': {
+            return {...state, counter: 0}
+        }
+        default: {
+            console.error(action, 'no action match')
+            return state
+        }
+    }
+}
+
+const store = createStore(reducer)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <Provider store={store}>
+          <App />
+      </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
